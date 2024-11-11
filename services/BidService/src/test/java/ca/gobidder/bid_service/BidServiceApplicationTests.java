94886d0546c86c37993939d0d8452d7f234e5420
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-11-11 15:11:09 -0500

    Create BidService

---
+++ services/BidService/src/test/java/ca/gobidder/bid_service/BidServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gobidder.bid_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
