f6c76840be1ef3ee7e66c9ca63546bde61f2472a
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-11-19 20:46:53 -0500

    Bid Service: Creates configs for Grpc, Kafka, Redis

---
+++ services/BidService/src/main/java/com/gobidder/bid_service/config/GrpcConfig.java
@@ -0,0 +1,21 @@
+package com.gobidder.bid_service.config;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class GrpcConfig {
+    @Value("{grpc.auction.host:auction-service}")
+    private String host;
+
+    @Value("${grpc.auction.port:9090}")
+    private int port;
+
+    @Bean
+    public ManagedChannel managedChannel() {
+        return ManagedChannelBuilder.forAddress("bid-service", 9090).usePlaintext().build();
+    }
+}
