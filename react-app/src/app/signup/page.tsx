e5705a30e7f59a5b47892d8fe29604842be6e90d
goIsaiah <goisaiah@outlook.com>
2024-12-14 23:08:36 -0500

    Fixed alignment for sign up, need to do the same for login

--- react-app/src/app/signup/page.tsx
+++ react-app/src/app/signup/page.tsx
@@ -15,64 +15,66 @@ import { Label } from "@/components/ui/label";
 
 export default function Home() {
   return (
-    <Card className="w-full max-w-4xl mx-auto p-6">
-      <CardHeader>
-        <CardTitle>Sign Up</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          {/* Two-column layout */}
-          <div className="grid grid-cols-2 gap-8">
-            {/* Left Column */}
-            <div className="flex flex-col space-y-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="firstName">First Name</Label>
-                <Input id="firstName" placeholder="" />
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <Card className="w-full max-w-4xl p-6">
+        <CardHeader>
+          <CardTitle>Sign Up</CardTitle>
+          <CardDescription>Deploy your new project in one-click.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            {/* Two-column layout */}
+            <div className="grid grid-cols-2 gap-8">
+              {/* Left Column */}
+              <div className="flex flex-col space-y-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="firstName">First Name</Label>
+                  <Input id="firstName" placeholder="" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="lastName">Last Name</Label>
+                  <Input id="lastName" placeholder="" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="streetAddress">Street Address</Label>
+                  <Input id="streetAddress" placeholder="" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="streetNumber">Street Number</Label>
+                  <Input id="streetNumber" placeholder="" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="postalCode">Postal Code</Label>
+                  <Input id="postalCode" placeholder="" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="city">City</Label>
+                  <Input id="city" placeholder="" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="country">Country</Label>
+                  <Input id="country" placeholder="" />
+                </div>
               </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="lastName">Last Name</Label>
-                <Input id="lastName" placeholder="" />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="streetAddress">Street Address</Label>
-                <Input id="streetAddress" placeholder="" />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="streetNumber">Street Number</Label>
-                <Input id="streetNumber" placeholder="" />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="postalCode">Postal Code</Label>
-                <Input id="postalCode" placeholder="" />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="city">City</Label>
-                <Input id="city" placeholder="" />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="country">Country</Label>
-                <Input id="country" placeholder="" />
+              {/* Right Column */}
+              <div className="flex flex-col space-y-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="username">Username</Label>
+                  <Input id="username" placeholder="" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="password">Password</Label>
+                  <Input id="password" type="password" placeholder="" />
+                </div>
               </div>
             </div>
-            {/* Right Column */}
-            <div className="flex flex-col space-y-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="username">Username</Label>
-                <Input id="username" placeholder="" />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" placeholder="" />
-              </div>
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Go Back</Button>
-        <Button>Submit</Button>
-      </CardFooter>
-    </Card>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Go Back</Button>
+          <Button>Submit</Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
