cac443b71cf5d4fc5a779ca48b73aa4f7676fd34
goIsaiah <goisaiah@outlook.com>
2024-12-14 23:05:55 -0500

    Created login and sign up pages

--- react-app/src/app/login/page.tsx
+++ react-app/src/app/login/page.tsx
@@ -34,44 +34,29 @@ const formSchema = z.object({
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button className="row-start-1">Hello, world!</Button>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Card className="w-[350px]">
-          <CardHeader>
-            <CardTitle>Create project</CardTitle>
-            <CardDescription>Deploy your new project in one-click.</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <div className="grid w-full items-center gap-4">
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="name">Name</Label>
-                  <Input id="name" placeholder="Name of your project" />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="framework">Framework</Label>
-                  <Select>
-                    <SelectTrigger id="framework">
-                      <SelectValue placeholder="Select" />
-                    </SelectTrigger>
-                    <SelectContent position="popper">
-                      <SelectItem value="next">Next.js</SelectItem>
-                      <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                      <SelectItem value="astro">Astro</SelectItem>
-                      <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-              </div>
-            </form>
-          </CardContent>
-          <CardFooter className="flex justify-between">
-            <Button variant="outline">Cancel</Button>
-            <Button>Deploy</Button>
-          </CardFooter>
-        </Card>
-      </main>
-    </div>
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Sign In</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Username</Label>
+              <Input id="name" placeholder="" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Password</Label>
+              <Input id="name" placeholder="" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Go Back</Button>
+        <Button>Submit</Button>
+      </CardFooter>
+    </Card>
   );
 }
---
+++ react-app/src/app/signup/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function Home() {
+  return (
+    <Card className="w-full max-w-4xl mx-auto p-6">
+      <CardHeader>
+        <CardTitle>Sign Up</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          {/* Two-column layout */}
+          <div className="grid grid-cols-2 gap-8">
+            {/* Left Column */}
+            <div className="flex flex-col space-y-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="firstName">First Name</Label>
+                <Input id="firstName" placeholder="" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="lastName">Last Name</Label>
+                <Input id="lastName" placeholder="" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="streetAddress">Street Address</Label>
+                <Input id="streetAddress" placeholder="" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="streetNumber">Street Number</Label>
+                <Input id="streetNumber" placeholder="" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="postalCode">Postal Code</Label>
+                <Input id="postalCode" placeholder="" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="city">City</Label>
+                <Input id="city" placeholder="" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="country">Country</Label>
+                <Input id="country" placeholder="" />
+              </div>
+            </div>
+            {/* Right Column */}
+            <div className="flex flex-col space-y-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" placeholder="" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" placeholder="" />
+              </div>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Go Back</Button>
+        <Button>Submit</Button>
+      </CardFooter>
+    </Card>
+  );
+}
