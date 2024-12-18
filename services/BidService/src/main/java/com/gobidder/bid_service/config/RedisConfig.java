94c35526de4996f84f738cc4a3c7ff9d59e5d0d8
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-12-18 03:28:48 -0500

    BidService: Updates RedisCacheModel to be serialisable

--- services/BidService/src/main/java/com/gobidder/bid_service/config/RedisConfig.java
+++ services/BidService/src/main/java/com/gobidder/bid_service/config/RedisConfig.java
@@ -4,13 +4,41 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.gobidder.bid_service.model.AuctionCacheModel;
 
 @Configuration
 public class RedisConfig {
+
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory, ObjectMapper objectMapper) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
+        template.setConnectionFactory(connectionFactory);
+
+        // config json serializer and use string serializer for keys
+        Jackson2JsonRedisSerializer<Object> jsonSerializer = new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+
+        template.setKeySerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+        template.setHashValueSerializer(jsonSerializer);
+        template.setValueSerializer(jsonSerializer);
+        template.setEnableDefaultSerializer(false);
+
+        template.afterPropertiesSet();
+
         return template;
     }
 }
