54e2ab741998679845130e10d4039cdb4bfa2f29
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-12-18 02:11:40 -0500

    BidService: Adds grpc-census to pom.xml and enables scheduling in main application

--- services/BidService/src/main/java/com/gobidder/bid_service/BidServiceApplication.java
+++ services/BidService/src/main/java/com/gobidder/bid_service/BidServiceApplication.java
@@ -2,8 +2,10 @@ package com.gobidder.bid_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BidServiceApplication {
 
 	public static void main(String[] args) {
